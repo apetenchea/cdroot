4185b6f6e97bf9a6b02a1bb3e78f2b9bb99a1453
Alex Petenchea <alex.petenchea@gmail.com>
2022-11-27 22:00:50 +0200

    Created NTP animation

--- source/_posts/time-in-distributed-systems.md
+++ source/_posts/time-in-distributed-systems.md
@@ -9,10 +9,10 @@ Operating Systems do a pretty good job at keeping track of time. When on the sam
 single source of truth: the system clock (also known as "kernel clock"). This is a software counter based on the timer
 interrupt. On Linux systems, it counts the number of seconds elapsed since Jan 1, 1970 (UTC).
 This can only function while the machine is running, so there's another clock (the
-[RTC](https://en.wikipedia.org/wiki/Real-time_clock)), which keeps track of time while the system is turned off,
-generally using a crystal oscillator. At boot time, the system clock is initialized from the RTC.  
-Keeping track of time is essential for scheduling events, measuring performance, debugging, etc. In a distributed
-system, a physical clock can be used to count seconds, while a logical clocks can be used to count events.
+[RTC](https://en.wikipedia.org/wiki/Real-time_clock)), which keeps track of time while the system is turned off.
+At boot time, the system clock is initialized from the RTC.  
+Keeping track of time is essential for scheduling events, measuring performance, debugging, etc. In distributed
+systems, physical clocks can be used to count seconds, while a logical clocks can be used to count events.
 
 ## Physical Clocks
 
@@ -21,7 +21,7 @@ system, a physical clock can be used to count seconds, while a logical clocks ca
 These can be found in any computer or mobile phone. The most commonly used type of physical clocks are quartz clocks,
 because they're really cheap and provide decent accuracy. The timekeeping component of every quartz clock is a [quartz
 crystal resonator](https://en.wikipedia.org/wiki/Quartz_clock#/media/File:Inside_QuartzCrystal-Tuningfork.jpg),
-in the shape of a musician's tuning fork. Via an electronic circuit, a battery sends electricity to the quartz crystal,
+in the shape of a musician's tuning fork. Via an electronic circuit, a battery sends electricity to the crystal,
 causing its prongs to vibrate at a certain frequency, generally 32768 Hz, or in other words, 32768 times per second.
 Counting the number of vibration allows us to count the number of seconds elapsed. This is not that relevant for a
 distributed system, but I find it very interesting.     
@@ -34,12 +34,17 @@ on the quantum mechanical properties of the caesium atom. Thus, we are able to c
 [IS](https://en.wikipedia.org/wiki/International_System_of_Units). Atomic clocks are way more expensive and therefore,
 not as widespread as quartz clocks.     
 For our perspective, a second is roughly the 31.557.600<sup>th</sup> part of a year. We think of a year as the time it takes to our
-planet to complete one revolution around the sun. While our interpretation of a second seems much nicer in day-to-day
+planet to complete one revolution around the sun. While our interpretation of a second works nicely in day-to-day
 life, the speed of Earth is not constant. [Coordinated Universal Time](https://en.wikipedia.org/wiki/Coordinated_Universal_Time)
 (or UTC) is based on atomic time, but includes corrections to account for variations in Earth's rotation. [Such
 adjustments](https://en.wikipedia.org/wiki/Leap_second) to UTC complicate software that needs to work with time and
 dates.
 
+### Network Time Protocol
+
+Even if most computers don't come with atomic clocks, they can periodically retrieve the current time from a server that
+has one. Unfortunately, in order to correctly adjust the clock, one has to take network latency and processing time into
+account. This is what the [Network Time Protocol](https://datatracker.ietf.org/doc/html/rfc5905) (NTP) is for.
 
 ## References
 
---
+++ source/_posts/time-in-distributed-systems/code/ntp-illustration.py
@@ -0,0 +1,103 @@
+from manim import *
+
+class NtpIllustration(Scene):
+    def construct(self):
+        client_x = -2
+        server_x = 2
+        t = [2.5, 0.5, -0.5, -2.5]
+
+        # Draw client and server
+        d1 = Dot([client_x, t[0], 0])
+        client_label = Text("Client").next_to(d1, UP)
+        d2 = Dot([server_x, t[0], 0])
+        server_label = Text("Server").next_to(d2, UP)
+        self.add(d1, client_label, d2, server_label)
+        self.wait()
+
+        # Draw t1 -> t2
+        d3 = Dot([server_x, t[1], 0])
+        d2_d3_line = Line(d2.get_center(), d2.get_center(), color=BLUE)
+        d1_d3_arrow = Arrow(d1.get_center(), d3.get_center(), color=BLUE)
+        self.add(d3, d2_d3_line)
+        t1_label = MathTex("t_1", color=GREEN).next_to(d1, LEFT)
+        anim1 = d2_d3_line.animate.put_start_and_end_on(d2.get_center(), d3.get_center())
+        anim2 = GrowArrow(d1_d3_arrow)
+        self.play(anim1, anim2, FadeIn(t1_label))
+        self.wait()
+        b1 = Brace(d2_d3_line, direction=RIGHT)
+        b1text = b1.get_text("Request time")
+        self.play(GrowFromCenter(b1), FadeIn(b1text))
+        self.wait()
+        t2_label = MathTex("t_2", color=BLUE).next_to(d3, RIGHT)
+        self.play(FadeOut(b1), FadeOut(b1text))
+        self.play(FadeIn(t2_label))
+        self.wait()
+
+        # Draw t2 -> t3
+        d4 = Dot([server_x, t[2], 0])
+        d3_d4_line = Line(d3.get_center(), d3.get_center(), color=RED)
+        self.add(d4, d3_d4_line)
+        anim3 = d3_d4_line.animate.put_start_and_end_on(d3.get_center(), d4.get_center())
+        self.play(anim3)
+        self.wait()
+        b2 = Brace(d3_d4_line, direction=LEFT)
+        b2text = b2.get_text("Processing time")
+        self.play(GrowFromCenter(b2), FadeIn(b2text))
+        self.wait()
+        t3_label = MathTex("t_3", color=RED).next_to(d4, RIGHT)
+        self.play(FadeOut(b2), FadeOut(b2text))
+        self.play(FadeIn(t3_label))
+        self.wait()
+
+        # Draw t3 -> t4
+        d5 = Dot([client_x, t[3], 0])
+        d6 = Dot([server_x, t[3], 0])
+        d1_d5_line = Line(d1.get_center(), d1.get_center(), color=GREEN)
+        d4_d6_line = Line(d4.get_center(), d4.get_center(), color=BLUE)
+        d4_d5_arrow = Arrow(d4.get_center(), d5.get_center(), color=BLUE)
+        self.add(d5, d1_d5_line, d6, d4_d6_line)
+        anim1 = d1_d5_line.animate.put_start_and_end_on(d1.get_center(), d5.get_center())
+        anim2 = d4_d6_line.animate.put_start_and_end_on(d4.get_center(), d6.get_center())
+        anim3 = GrowArrow(d4_d5_arrow)
+        self.play(anim1, anim2, anim3)
+        self.wait()
+        b3 = Brace(d4_d6_line, direction=RIGHT)
+        b3text = b3.get_text("Response time")
+        self.play(FadeOut(t3_label), GrowFromCenter(b3), FadeIn(b3text))
+        self.wait()
+        t4_label = MathTex("t_4", color=GREEN).next_to(d5, LEFT)
+        self.play(FadeOut(b3), FadeOut(b3text), FadeIn(t4_label), FadeIn(t3_label))
+        self.wait()
+
+        # Translate
+        group = VGroup(d3, d4, d3_d4_line, t2_label, t3_label)
+        self.play(group.animate.shift(LEFT * (server_x - client_x)), FadeOut(d1_d3_arrow), FadeOut(d4_d5_arrow),
+                  FadeOut(server_label), FadeOut(d2), FadeOut(d6), FadeOut(d4_d6_line), FadeOut(d2_d3_line),
+                  FadeOut(client_label))
+        self.wait()
+
+        # Rotate
+        group = VGroup(d3, d4, d3_d4_line, t2_label, t3_label, t1_label, t4_label, d1_d5_line, d1, d5)
+        self.add_foreground_mobjects(d3_d4_line)
+        t2_label.next_to(d3, LEFT)
+        t3_label.next_to(d4, LEFT)
+        self.play(group.animate.shift(RIGHT * ((server_x - client_x) / 2)))
+        self.wait(0.5)
+        self.play(Rotate(group, angle=PI/2))
+        rotation = [
+            Rotate(t1_label, angle=-PI/2),
+            Rotate(t2_label, angle=-PI/2),
+            Rotate(t3_label, angle=-PI/2),
+            Rotate(t4_label, angle=-PI/2),
+        ]
+        self.play(*rotation)
+        self.wait()
+
+        # Explain
+        delta = MathTex(r"\Delta = (t_4 - t_1) - (t_3 - t2)")
+        delta.next_to(d3_d4_line, UP * 8)
+        theta = MathTex(r"\Theta = t_3 + \frac{\Delta}{2} - t_4")
+        theta.next_to(delta, DOWN)
+        self.play(Write(delta))
+        self.play(Write(theta))
+        self.wait()
