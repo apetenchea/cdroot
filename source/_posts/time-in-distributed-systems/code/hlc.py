a1db6e64ce03149f0fabf16f1674cea4bab4c055
Alex Petenchea <alex.petenchea@gmail.com>
2023-04-15 20:51:22 +0300

    more compact code

--- source/_posts/time-in-distributed-systems/code/hlc.py
+++ source/_posts/time-in-distributed-systems/code/hlc.py
@@ -25,7 +25,8 @@ class HLC:
         """
         physical = self.get_physical_time()
         if physical <= self._last_timestamp.physical:
-            # Physical time stays the same, but the logical component gets updated.
+            # Physical time stays the same,
+            # but the logical component gets updated.
             physical = self._last_timestamp.physical
             logical = self._last_timestamp.logical + 1
         else:
@@ -37,21 +38,26 @@ class HLC:
         """
         Adjust the local clock based on a received timestamp and return a new timestamp.
         """
-        physical = max(self.get_physical_time(), self._last_timestamp.physical, received.physical)
+        physical = max(self.get_physical_time(),
+                       self._last_timestamp.physical,
+                       received.physical)
         if physical == self._last_timestamp.physical:
             if physical == received.physical:
-                # The last recorded timestamp and the received timestamp have the same physical time,
-                # greater than the current physical time.
+                # The last recorded timestamp and the received timestamp
+                # have the same physical time, greater than the current physical time.
                 logical = max(self._last_timestamp.logical, received.logical) + 1
             else:
-                # The last recorded timestamp has a higher physical time than the received timestamp.
+                # The last recorded timestamp has a higher physical time
+                # than the received timestamp.
                 logical = self._last_timestamp.logical + 1
         else:
             if physical == received.physical:
-                # The received timestamp has a higher physical time than the last recorded timestamp.
+                # The received timestamp has a higher physical time
+                # than the last recorded timestamp.
                 logical = received.logical + 1
             else:
-                # The current physical time is higher than both the last recorded timestamp and the received timestamp.
+                # The current physical time is higher than
+                # both the last recorded timestamp and the received timestamp.
                 logical = 0
         self._last_timestamp = Timestamp(physical, logical)
         return self._last_timestamp
